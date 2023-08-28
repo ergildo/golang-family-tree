4c8315874714b1004ef4e32e6288f6a3e910bf13
ergildo.dias <Ergildo.Dias@vwac.net>
2023-08-28 22:39:47 +0200

    add initial load script

---
+++ migrations/00003_insert_person_table.down.sql
@@ -0,0 +1,2 @@
+delete from relationship where parentid <14 and childid <14;
+delete from person where id < 14;
---
+++ migrations/00003_insert_person_table.up.sql
@@ -0,0 +1,7 @@
+INSERT INTO person ("name") VALUES
+                                ('Sony'),('Martin'),('Anastasia'),('Ellen'),('Oprah'),
+                                ('Mike'),('Phoebe'),('Ursula'),('Eric'), ('Ariel'),
+                                ('Duny'),('Bruce'),('Jaqueline'),('Melody');
+INSERT INTO relationship (parentId,childId) VALUES
+                                                (1,6),(2,7),(3,7),(2,8),(3,8),(4,9),(5,9),
+                                                (6,11), (6,12), (7, 11), (7,12), (8,13), (9,13), (9,14), (10, 14);
